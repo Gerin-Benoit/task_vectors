0d2c2a1ce0db3f387537e2523cdfcd869442f9dd
Gerin-Benoit <benoit.gerin@uclouvain.be>
2024-04-11 16:02:02 +0200

    example

--- src/example.py
+++ src/example.py
@@ -4,23 +4,44 @@ from eval import eval_single_dataset
 from args import parse_arguments
 
 # Config
-dataset = 'MNIST'
+datasets = ['MNIST', 'CIFAR100']
 model = 'ViT-L-14'
 args = parse_arguments()
 args.data_location = '/export/DATA/gerinb/task-vectors/datasets'
 args.model = model
 args.save = f'checkpoints/{model}'
 pretrained_checkpoint = f'checkpoints/{model}/zeroshot.pt'
-finetuned_checkpoint = f'checkpoints/{model}/{dataset}/finetuned.pt'
 
-
-# Create the task vector
-task_vector = TaskVector(pretrained_checkpoint, finetuned_checkpoint)
-# Negate the task vector
-neg_task_vector = -task_vector
-# Apply the task vector
-image_encoder = neg_task_vector.apply_to(pretrained_checkpoint, scaling_coef=0.5)
+# Create the task vectors
+task_vectors = [
+    TaskVector(pretrained_checkpoint, f'checkpoints/{model}/{dataset}/finetuned.pt')
+    for dataset in datasets
+]
+# Sum the task vectors
+task_vector_sum = sum(task_vectors)
+# Apply the resulting task vector
+image_encoder = task_vector_sum.apply_to(pretrained_checkpoint, scaling_coef=0.8)
 # Evaluate
-eval_single_dataset(image_encoder, dataset, args)
-args.data_location = 'datasets/data'
-eval_single_dataset(image_encoder, 'ImageNet', args)
+print("Sources ZS:")
+for dataset in datasets:
+    print(dataset)
+    eval_single_dataset(pretrained_checkpoint, dataset, args)
+
+print("Sources finetuned:")
+for i, dataset in enumerate(datasets):
+    print(dataset)
+    eval_single_dataset(task_vectors[i].apply_to(pretrained_checkpoint, scaling_coef=1), dataset, args)
+
+print("Sources with task-vectors combination:")
+for dataset in datasets:
+    print(dataset)
+    eval_single_dataset(image_encoder, dataset, args)
+
+print("ImageNet ZS:")
+args.data_location = "datasets/data"
+eval_single_dataset(pretrained_checkpoint, "ImageNet", args)
+
+print("ImageNet with task vectors combination:")
+
+eval_single_dataset(image_encoder, "ImageNet", args)
+

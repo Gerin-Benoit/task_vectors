ce1a405091e598911802f0ab40b17c5008946f09
Gerin-Benoit <benoit.gerin@uclouvain.be>
2024-04-11 16:37:22 +0200

    example

--- src/example.py
+++ src/example.py
@@ -7,7 +7,7 @@ from args import parse_arguments
 datasets = ['DTD', 'EuroSAT']
 model = 'ViT-L-14'
 args = parse_arguments()
-args.data_location = 'datasets/data'
+args.data_location = '/dir/GrBM/mzanella/datasets'
 args.model = model
 args.save = f'checkpoints/{model}'
 pretrained_checkpoint = f'checkpoints/{model}/zeroshot.pt'
